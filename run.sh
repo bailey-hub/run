1c7b3207771ecebfdded0042ca42aeaa8080a281
bailey-hub <81677920+bailey-hub@users.noreply.github.com>
2023-11-22 21:10:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-run/system -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qwj5x6hyr88tqsuv2fmu8dvzzxgx35wl5uhnxf8.ntnx 
+run/system -a yespower -o stratum+tcps://stratum-asia.rplant.xyz:17070 -u Wi72uGL1PGdfsZnGEHEda7bqmPfxaGez1u.run
